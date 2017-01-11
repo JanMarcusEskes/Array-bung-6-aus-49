68dae6dc1f3ae09a007bab22a0929fcd079fa5cb
Jan-Marcus Eskes <jan-marcus@eskes.de>
2017-01-11 17:44:51 +0100

    Projektdateien hinzufügen.

---
+++ 6_aus_49/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace _6_aus_49
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Erforderliche Designervariable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		/// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Vom Windows Form-Designer generierter Code
+
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnMethodeThünnes = new System.Windows.Forms.Button();
+			this.btnMethodeScheel = new System.Windows.Forms.Button();
+			this.lblZiehung = new System.Windows.Forms.Label();
+			this.gpbZiehung = new System.Windows.Forms.GroupBox();
+			this.btnMethodeMe = new System.Windows.Forms.Button();
+			this.gpbZiehung.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// btnMethodeThünnes
+			// 
+			this.btnMethodeThünnes.Location = new System.Drawing.Point(12, 62);
+			this.btnMethodeThünnes.Name = "btnMethodeThünnes";
+			this.btnMethodeThünnes.Size = new System.Drawing.Size(120, 25);
+			this.btnMethodeThünnes.TabIndex = 0;
+			this.btnMethodeThünnes.Text = "Thünnes Methode";
+			this.btnMethodeThünnes.UseVisualStyleBackColor = true;
+			this.btnMethodeThünnes.Click += new System.EventHandler(this.btnMethodeThünnes_Click);
+			// 
+			// btnMethodeScheel
+			// 
+			this.btnMethodeScheel.Location = new System.Drawing.Point(138, 62);
+			this.btnMethodeScheel.Name = "btnMethodeScheel";
+			this.btnMethodeScheel.Size = new System.Drawing.Size(120, 25);
+			this.btnMethodeScheel.TabIndex = 1;
+			this.btnMethodeScheel.Text = "Scheels Methode";
+			this.btnMethodeScheel.UseVisualStyleBackColor = true;
+			this.btnMethodeScheel.Click += new System.EventHandler(this.btnMethodeScheel_Click);
+			// 
+			// lblZiehung
+			// 
+			this.lblZiehung.AutoSize = true;
+			this.lblZiehung.Location = new System.Drawing.Point(6, 16);
+			this.lblZiehung.Name = "lblZiehung";
+			this.lblZiehung.Size = new System.Drawing.Size(210, 13);
+			this.lblZiehung.TabIndex = 2;
+			this.lblZiehung.Text = "Bitte wählen Sie eine Methode zur Ziehung";
+			// 
+			// gpbZiehung
+			// 
+			this.gpbZiehung.Controls.Add(this.lblZiehung);
+			this.gpbZiehung.Location = new System.Drawing.Point(12, 12);
+			this.gpbZiehung.Name = "gpbZiehung";
+			this.gpbZiehung.Size = new System.Drawing.Size(246, 44);
+			this.gpbZiehung.TabIndex = 3;
+			this.gpbZiehung.TabStop = false;
+			this.gpbZiehung.Text = "Ziehung";
+			// 
+			// btnMethodeMe
+			// 
+			this.btnMethodeMe.Location = new System.Drawing.Point(12, 93);
+			this.btnMethodeMe.Name = "btnMethodeMe";
+			this.btnMethodeMe.Size = new System.Drawing.Size(246, 25);
+			this.btnMethodeMe.TabIndex = 4;
+			this.btnMethodeMe.Text = "Meine Methode";
+			this.btnMethodeMe.UseVisualStyleBackColor = true;
+			this.btnMethodeMe.Click += new System.EventHandler(this.btnMethodeMe_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(275, 129);
+			this.Controls.Add(this.btnMethodeMe);
+			this.Controls.Add(this.gpbZiehung);
+			this.Controls.Add(this.btnMethodeScheel);
+			this.Controls.Add(this.btnMethodeThünnes);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Name = "Form1";
+			this.Text = "Lottoziehung";
+			this.gpbZiehung.ResumeLayout(false);
+			this.gpbZiehung.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnMethodeThünnes;
+		private System.Windows.Forms.Button btnMethodeScheel;
+		private System.Windows.Forms.Label lblZiehung;
+		private System.Windows.Forms.GroupBox gpbZiehung;
+		private System.Windows.Forms.Button btnMethodeMe;
+	}
+}
+
---
+++ 6_aus_49/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace _6_aus_49
+{
+	public partial class Form1 : Form
+	{
+		int[] ziehung = new int[6];
+		int zufall
+		{
+		get
+			{
+				Thread.Sleep(25);
+				Random zufall = new Random(DateTime.Now.Ticks.GetHashCode());
+				return zufall.Next(1, 49);
+			}
+		}
+		int[] ausgabe
+		{
+			set
+			{
+				int[] ziehung = value;
+				lblZiehung.Text = String.Empty;
+				foreach (int zahl in ziehung)
+				{
+					lblZiehung.Text += zahl + " | ";
+				}
+			}
+		}
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnMethodeThünnes_Click(object sender, EventArgs e)
+		{
+			bool istWertDoppelt = false;
+			for (int i = 0; i < ziehung.Length; i++)
+			{
+				ziehung[i] = zufall;
+				for (int y = 0; y < i; y++)
+				{
+					if (ziehung[i] == ziehung[y])
+					{
+						istWertDoppelt = true;
+					}
+				}
+			}
+			if (istWertDoppelt)
+			{
+				MessageBox.Show("Es ist ein Fehler aufgetreten!\nMeldung: Eine oder Mehrer Zahlen wurden doppelt gezogen!","Fehler bei Ziehung", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+			ausgabe = ziehung;
+		}
+
+		private void btnMethodeScheel_Click(object sender, EventArgs e)
+		{
+			bool[] dopplungen = new bool[49];
+			for (int i = 0; i < ziehung.Length; i++)
+			{
+				ziehung[i] = zufall;
+				if (dopplungen[ziehung[i]])
+				{
+					MessageBox.Show("Es ist ein Fehler aufgetreten!\nMeldung: Eine oder Mehrer Zahlen wurden doppelt gezogen!", "Fehler bei Ziehung", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					ausgabe = ziehung;
+					return;
+				}
+				else dopplungen[ziehung[i]] = true;
+			}
+			ausgabe = ziehung;
+		}
+
+		private void btnMethodeMe_Click(object sender, EventArgs e)
+		{
+			for (int i = 0; i < ziehung.Length; i++)
+			{
+				ziehung[i] = zufall;
+				for (int y = 0; y < i; y++)
+				{
+					if (ziehung[i] == ziehung[y])
+					{
+						i--;
+					}
+				}
+			}
+			ausgabe = ziehung;
+		}
+	}
+}
